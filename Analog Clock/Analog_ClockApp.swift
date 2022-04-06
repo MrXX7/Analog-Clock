05be9ec7f002a2fc2f563f8b4d340231322f09ce
Oncu <oohancan@gmail.com>
2022-04-06 18:46:11 +0300

    Initial Commit

---
+++ Analog Clock/Analog_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Analog_ClockApp.swift
+//  Analog Clock
+//
+//  Created by Oncu Ohancan on 6.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Analog_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
